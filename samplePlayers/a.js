af5cad1697e11b3e9de26334297faa403bad6cb8
Greg Skiano <gregoryskiano@gmail.com>
2015-06-26 19:28:42 -0400

    making ids better

--- samplePlayers/a.js
+++ samplePlayers/a.js
@@ -2,7 +2,7 @@
 export default (function makePlayer() {
   // what cards do you have
   // what is the card on the table
-  return function(isPlaying, state) {
+  return function myStrategy(isPlaying, state) {
     if (isPlaying) {
       var play = state.hand.pop();
       return parseInt(play) >= parseInt(state.table) ? play : null;
