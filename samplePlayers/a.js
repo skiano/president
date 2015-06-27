0ce51afe51323308dea4e5d8a1196d8f44760123
Greg Skiano <gregoryskiano@gmail.com>
2015-06-27 13:23:39 -0400

    imporving testing display

--- samplePlayers/a.js
+++ samplePlayers/a.js
@@ -4,7 +4,6 @@ export default (function makePlayer() {
   // what is the card on the table
   return function myStrategy(isPlaying, state) {
     if (isPlaying) {
-      console.log(state.hand);
       var play = state.hand.pop();
       return parseInt(play) >= parseInt(state.table) ? play : null;
     }
