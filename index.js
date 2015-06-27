65c9dd250b0c79849d2c3be32a4a884993fdcc35
Greg Skiano <gregoryskiano@gmail.com>
2015-06-26 22:24:15 -0400

    using args instead of array

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("babel/register");
 var game = require('./lib');
 var player = require('./samplePlayers/a');
 
-const results = game([player, player, player, player, player]);
+const results = game(player, player, player, player, player);
 
 console.log(results);
 
