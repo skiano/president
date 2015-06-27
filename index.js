9430d5144f89db83ddb614baecf580a6208e9b57
Greg Skiano <gregoryskiano@gmail.com>
2015-06-27 13:22:25 -0400

    imporving testing display

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 
 require("babel/register");
 
-var game = require('./lib');
-var player = require('./samplePlayers/a');
+const game = require('./lib');
+const player = require('./samplePlayers/a');
+const results = game(player, player, player, player, player, player);
 
-const results = game(player, player, player, player, player);
+// http://unix.stackexchange.com/questions/43408/printing-colored-text-using-echo
+const colors = [
+  '\033[31m',
+  '\033[34m',
+  '\033[33m',
+  '\033[32m',
+  '\033[36m',
+  '\033[33m',
+  '\033[35m',
+  '\033[37m'
+]
 
-console.log(results);
+const colorEnd = '\033[0m';
+
+results.events.forEach(function (r) {
+  const player = getPlayerString(r.player);
+  const sortedHand = sortHand(r.hand);
+  const play = getPlay(r.play, r.finished);
+  console.log(player, '|', play, '|', sortedHand);
+});
+
+function getPlay(play, finished) {
+  play = play || '-';
+  play = paddLeft(play)
+  return finished ? (colors[0] + play + colorEnd) : play;
+}
+
+function getPlayerString(player) {
+  const color = colors[results.rank.indexOf(player)];
+  return color + player + colorEnd;
+}
+
+// http://stackoverflow.com/questions/2686855/is-there-a-javascript-function-that-can-pad-a-string-to-get-to-a-determined-leng
+function paddLeft(str, l, char) {
+  l = l || 10;
+  char = char || ' ';
+  const padd = char.repeat(l);
+  // left
+  // return (padd + str).slice(-padd.length); 
+  return (str + padd).substring(0, padd.length)
+}
+
+function sortHand(hand) {
+  return hand.split().sort(function (a, b) {
+    return parseInt(a) > parseInt(b);
+  }).join();
+}
 
--- samplePlayers/a.js
+++ samplePlayers/a.js
@@ -4,6 +4,7 @@ export default (function makePlayer() {
   // what is the card on the table
   return function myStrategy(isPlaying, state) {
     if (isPlaying) {
+      console.log(state.hand);
       var play = state.hand.pop();
       return parseInt(play) >= parseInt(state.table) ? play : null;
     }
